1efb26b647e11682dd12e6b338272be00fb72d6a
Matteo Collina <hello@matteocollina.com>
2017-12-24 17:23:28 +0100

    Do not use class

--- index.js
+++ index.js
@@ -4,98 +4,102 @@ const fs = require('fs')
 const EventEmitter = require('events')
 const reusify = require('reusify')
 const flatstr = require('flatstr')
+const inherits = require('util').inherits
 
-class Holder {
-  constructor () {
-    this.sonic = null
-    var that = this
-    this.release = function (err) {
-      var sonic = that.sonic
-      if (err) {
-        sonic.emit('error', err)
-        return
-      }
+function Holder () {
+  EventEmitter.call(this)
 
-      pool.release(that)
+  this.sonic = null
+  var that = this
+  this.release = function (err) {
+    var sonic = that.sonic
+    if (err) {
+      sonic.emit('error', err)
+      return
+    }
 
-      if (sonic._buf.length > 0) {
-        actualWrite(sonic)
-      } else if (sonic._ending === true) {
-        actualClose(sonic)
-      } else {
-        sonic._writing = false
-        sonic.emit('drain')
-      }
+    pool.release(that)
+
+    if (sonic._buf.length > 0) {
+      actualWrite(sonic)
+    } else if (sonic._ending === true) {
+      actualClose(sonic)
+    } else {
+      sonic._writing = false
+      sonic.emit('drain')
     }
   }
 }
 
 const pool = reusify(Holder)
 
-class SonicBoom extends EventEmitter {
-  constructor (fd) {
-    super()
-    this._buf = ''
-    this.fd = -1
-    this._writing = false
-    this._ending = false
-    this.destroyed = false
+function SonicBoom (fd) {
+  if (!(this instanceof SonicBoom)) {
+    return new SonicBoom(fd)
+  }
 
-    if (typeof fd === 'number') {
+  this._buf = ''
+  this.fd = -1
+  this._writing = false
+  this._ending = false
+  this.destroyed = false
+
+  if (typeof fd === 'number') {
+    this.fd = fd
+  } else if (typeof fd === 'string') {
+    this._writing = true
+    fs.open(fd, 'a', (err, fd) => {
+      if (err) {
+        this.emit('error', err)
+        return
+      }
+
       this.fd = fd
-    } else if (typeof fd === 'string') {
-      this._writing = true
-      fs.open(fd, 'a', (err, fd) => {
-        if (err) {
-          this.emit('error', err)
-          return
-        }
+      this._writing = false
+      // start
+      if (this._buf.length > 0) {
+        actualWrite(this)
+      }
+    })
+  } else {
+    throw new Error('SonicBoom supports only file descriptors and files')
+  }
+}
 
-        this.fd = fd
-        this._writing = false
-        // start
-        if (this._buf.length > 0) {
-          actualWrite(this)
-        }
-      })
-    } else {
-      throw new Error('SonicBoom supports only file descriptors and files')
-    }
+inherits(SonicBoom, EventEmitter)
+
+SonicBoom.prototype.write = function (data) {
+  if (this.destroyed) {
+    throw new Error('SonicBoom destroyed')
+  }
+  this._buf += data
+  if (this._writing === false) {
+    actualWrite(this)
+  }
+  return this._buf.length < 16384
+}
+
+SonicBoom.prototype.end = function () {
+  if (this._writing === true) {
+    this._ending = true
+    return
   }
 
-  write (data) {
-    if (this.destroyed) {
-      throw new Error('SonicBoom destroyed')
-    }
-    this._buf += data
-    if (this._writing === false) {
-      actualWrite(this)
-    }
-    return this._buf.length < 16384
+  actualClose(this)
+}
+
+SonicBoom.prototype.flushSync = function () {
+  if (this.fd < 0) {
+    throw new Error('sonic boom is not ready yet')
   }
 
-  end () {
-    if (this._writing === true) {
-      this._ending = true
-      return
-    }
-
-    actualClose(this)
+  if (this._buf.length > 0) {
+    fs.writeSync(this.fd, this._buf, 'utf8')
   }
+}
 
-  flushSync () {
-    if (this.fd < 0) {
-      throw new Error('sonic boom is not ready yet')
-    }
-
-    if (this._buf.length > 0) {
-      fs.writeSync(this.fd, this._buf, 'utf8')
-    }
-  }
-
-  destroy () {
-    actualClose(this)
-  }
+SonicBoom.prototype.destroy = function () {
+  actualClose(this)
 }
 
 function actualWrite (sonic) {
