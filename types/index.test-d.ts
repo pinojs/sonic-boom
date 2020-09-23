b915330bcf89ba416fca7a513dc1dbc80965e54e
Igor Savin <iselwin@gmail.com>
2020-09-23 13:02:40 +0300

    Add "the infamous triplet"

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,10 +1,18 @@
-import { SonicBoom } from "../";
-import SonicBoomEsm from "../";
+import { SonicBoom as SonicBoomNamed } from "../";
+import SonicBoomDefault from "../";
 import * as SonicBoomStar from "../";
-const sonic = new SonicBoom(1)
-const sonic2 = new SonicBoomEsm(1)
+import SonicBoomCjsImport = require ("../");
+const SonicBoomCjs = require("../");
+const { SonicBoom } = require('SonicBoom')
+
+const sonic = new SonicBoomNamed(1)
+const sonic2 = new SonicBoomDefault(1)
 const sonic3 = new SonicBoomStar.SonicBoom(1)
 const sonic4 = new SonicBoomStar.default(1)
+const sonic5 = new SonicBoomCjsImport.SonicBoom(1)
+const sonic6 = new SonicBoomCjsImport.default(1)
+const sonic7 = new SonicBoomCjs(1);
+const sonic8 = new SonicBoom(1);
 
 sonic.write('hello sonic\n');
 
