b6b419458e72398a547f505753ed1c286f736f8d
Igor Savin <iselwin@gmail.com>
2020-09-23 13:21:08 +0300

    Improve assertion

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,18 +1,21 @@
-import { SonicBoom as SonicBoomNamed } from "../";
+import { expectType } from "tsd";
+import SonicBoom, { SonicBoom as SonicBoomNamed } from "../";
 import SonicBoomDefault from "../";
 import * as SonicBoomStar from "../";
 import SonicBoomCjsImport = require ("../");
 const SonicBoomCjs = require("../");
-const { SonicBoom } = require('SonicBoom')
+const { SonicBoom: SonicBoomCjsNamed } = require('SonicBoom')
 
-const sonic = new SonicBoomNamed(1)
-const sonic2 = new SonicBoomDefault(1)
-const sonic3 = new SonicBoomStar.SonicBoom(1)
-const sonic4 = new SonicBoomStar.default(1)
-const sonic5 = new SonicBoomCjsImport.SonicBoom(1)
-const sonic6 = new SonicBoomCjsImport.default(1)
-const sonic7 = new SonicBoomCjs(1);
-const sonic8 = new SonicBoom(1);
+const sonic = new SonicBoom(1);
+
+expectType<SonicBoom>(new SonicBoomNamed(1));
+expectType<SonicBoom>( new SonicBoomDefault(1));
+expectType<SonicBoom>( new SonicBoomStar.SonicBoom(1));
+expectType<SonicBoom>( new SonicBoomStar.default(1));
+expectType<SonicBoom>( new SonicBoomCjsImport.SonicBoom(1));
+expectType<SonicBoom>( new SonicBoomCjsImport.default(1));
+expectType<any>( new SonicBoomCjs(1));
+expectType<any>( new SonicBoomCjsNamed(1));
 
 sonic.write('hello sonic\n');
 
