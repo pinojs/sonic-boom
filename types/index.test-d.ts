df13924df536f05cbfaa18e4a1e44407d84162af
Frazer Smith <frazer.dev@outlook.com>
2022-11-05 07:19:16 +0000

    test(types): remove unused variable assignments (#159)

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -30,8 +30,8 @@ sonic.end();
 sonic.destroy();
 
 const extraSonic = new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, append: true, mode: 0o644, mkdir: true});
-const sonicDest = new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, append: true, mode: 0o644, mkdir: true, dest: '/dev/null'});
-const sonicDest2 = new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, append: true, mode: 0o644, mkdir: true, dest: 1});
-const sonicDest3 = new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, fsync: true, append: true, mode: 0o644, mkdir: true});
+new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, append: true, mode: 0o644, mkdir: true, dest: '/dev/null'});
+new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, append: true, mode: 0o644, mkdir: true, dest: 1});
+new SonicBoom({fd: 1, minLength: 0, maxWrite: 16384, sync: true, fsync: true, append: true, mode: 0o644, mkdir: true});
 
 extraSonic.write('extra sonic\n');
