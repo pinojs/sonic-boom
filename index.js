14db7166667ff4b9b731ea62d104d138871ba0fc
user <>
2024-08-05 15:56:04 +0300

    periodicFlush

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function SonicBoom (opts) {
     return new SonicBoom(opts)
   }
 
-  let { fd, dest, minLength, maxLength, maxWrite, sync, append = true, mkdir, retryEAGAIN, fsync, contentMode, mode } = opts || {}
+  let { fd, dest, minLength, maxLength, maxWrite, periodicFlush, sync, append = true, mkdir, retryEAGAIN, fsync, contentMode, mode } = opts || {}
 
   fd = fd || dest
 
@@ -114,6 +114,8 @@ function SonicBoom (opts) {
   this.minLength = minLength || 0
   this.maxLength = maxLength || 0
   this.maxWrite = maxWrite || MAX_WRITE
+  this._periodicFlush = periodicFlush || 0
+  this._periodicFlushIID = 0
   this.sync = sync || false
   this.writable = true
   this._fsync = fsync || false
@@ -242,6 +244,10 @@ function SonicBoom (opts) {
       this._asyncDrainScheduled = false
     }
   })
+
+  if (this._periodicFlush !== 0) {
+    this._periodicFlushIID = setInterval(() => this.flush(null), this._periodicFlush)
+  }
 }
 
 /**
@@ -644,6 +650,10 @@ function actualClose (sonic) {
   if (sonic.fd === -1) {
     sonic.once('ready', actualClose.bind(null, sonic))
     return
+  }
+
+  if (sonic._periodicFlushIID !== 0) {
+    clearInterval(sonic._periodicFlushIID)
   }
 
   sonic.destroyed = true
---
+++ test/periodicflush.test.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const fs = require('fs')
+const SonicBoom = require('../')
+const { file, runTests } = require('./helper')
+
+runTests(buildTests)
+
+function buildTests (test, sync) {
+  // Reset the umask for testing
+  process.umask(0o000)
+
+  test('periodicflush_off', (t) => {
+    t.plan(4)
+
+    const dest = file()
+    const fd = fs.openSync(dest, 'w')
+    const stream = new SonicBoom({ fd, sync, minLength: 5000 })
+
+    t.ok(stream.write('hello world\n'))
+
+    setTimeout(function () {
+      fs.readFile(dest, 'utf8', function (err, data) {
+        t.error(err)
+        t.equal(data, '')
+
+        stream.destroy()
+        t.pass('file empty')
+      })
+    }, 2000)
+  })
+
+  test('periodicflush_on', (t) => {
+    t.plan(4)
+
+    const dest = file()
+    const fd = fs.openSync(dest, 'w')
+    const stream = new SonicBoom({ fd, sync, minLength: 5000, periodicFlush: 1000 })
+
+    t.ok(stream.write('hello world\n'))
+
+    setTimeout(function () {
+      fs.readFile(dest, 'utf8', function (err, data) {
+        t.error(err)
+        t.equal(data, 'hello world\n')
+
+        stream.destroy()
+        t.pass('file not empty')
+      })
+    }, 2000)
+  })
+}
