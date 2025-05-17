d1c3086d331ab7963ba32762f2bcf4aed3ae5b8f
Luca Del Puppo <luca@delpuppo.net>
2025-05-17 14:27:20 +0200

    test: move mode.test.js to node test

--- test/mode.test.js
+++ test/mode.test.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 const SonicBoom = require('../')
-const { file, runTestsLegacy: runTests } = require('./helper')
+const { file, runTests, once } = require('./helper')
 
 const isWindows = process.platform === 'win32'
 
@@ -13,104 +13,104 @@ function buildTests (test, sync) {
   // Reset the umask for testing
   process.umask(0o000)
 
-  test('mode', { skip: isWindows }, (t) => {
-    t.plan(6)
+  test('mode', { skip: isWindows }, async (t) => {
+    t.plan(5)
 
     const dest = file()
     const mode = 0o666
     const stream = new SonicBoom({ dest, sync, mode })
 
-    stream.on('ready', () => {
-      t.pass('ready emitted')
+    const endPromise1 = once(stream, 'ready', () => {
+      t.assert.ok('ready emitted')
     })
 
-    t.ok(stream.write('hello world\n'))
-    t.ok(stream.write('something else\n'))
+    t.assert.ok(stream.write('hello world\n'))
+    t.assert.ok(stream.write('something else\n'))
 
     stream.end()
 
-    stream.on('finish', () => {
-      fs.readFile(dest, 'utf8', (err, data) => {
-        t.error(err)
-        t.equal(data, 'hello world\nsomething else\n')
-        t.equal(fs.statSync(dest).mode & 0o777, stream.mode)
-      })
+    const endPromise2 = once(stream, 'finish', () => {
+      const data = fs.readFileSync(dest, 'utf8')
+      t.assert.strictEqual(data, 'hello world\nsomething else\n')
+      t.assert.strictEqual(fs.statSync(dest).mode & 0o777, stream.mode)
     })
+
+    await Promise.all([endPromise1, endPromise2])
   })
 
-  test('mode default', { skip: isWindows }, (t) => {
-    t.plan(6)
+  test('mode default', { skip: isWindows }, async (t) => {
+    t.plan(5)
 
     const dest = file()
     const defaultMode = 0o666
     const stream = new SonicBoom({ dest, sync })
 
-    stream.on('ready', () => {
-      t.pass('ready emitted')
+    const endPromise1 = once(stream, 'ready', () => {
+      t.assert.ok('ready emitted')
     })
 
-    t.ok(stream.write('hello world\n'))
-    t.ok(stream.write('something else\n'))
+    t.assert.ok(stream.write('hello world\n'))
+    t.assert.ok(stream.write('something else\n'))
 
     stream.end()
 
-    stream.on('finish', () => {
-      fs.readFile(dest, 'utf8', (err, data) => {
-        t.error(err)
-        t.equal(data, 'hello world\nsomething else\n')
-        t.equal(fs.statSync(dest).mode & 0o777, defaultMode)
-      })
+    const endPromise2 = once(stream, 'finish', () => {
+      const data = fs.readFileSync(dest, 'utf8')
+      t.assert.strictEqual(data, 'hello world\nsomething else\n')
+      t.assert.strictEqual(fs.statSync(dest).mode & 0o777, defaultMode)
     })
+
+    await Promise.all([endPromise1, endPromise2])
   })
 
-  test('mode on mkdir', { skip: isWindows }, (t) => {
-    t.plan(5)
+  test('mode on mkdir', { skip: isWindows }, async (t) => {
+    t.plan(4)
 
     const dest = path.join(file(), 'out.log')
     const mode = 0o666
     const stream = new SonicBoom({ dest, mkdir: true, mode, sync })
 
-    stream.on('ready', () => {
-      t.pass('ready emitted')
+    const endPromise1 = once(stream, 'ready', () => {
+      t.assert.ok('ready emitted')
     })
 
-    t.ok(stream.write('hello world\n'))
+    t.assert.ok(stream.write('hello world\n'))
 
     stream.flush()
 
-    stream.on('drain', () => {
-      fs.readFile(dest, 'utf8', (err, data) => {
-        t.error(err)
-        t.equal(data, 'hello world\n')
-        t.equal(fs.statSync(dest).mode & 0o777, stream.mode)
-        stream.end()
-      })
+    const endPromise2 = once(stream, 'drain', () => {
+      const data = fs.readFileSync(dest, 'utf8')
+      t.assert.strictEqual(data, 'hello world\n')
+      t.assert.strictEqual(fs.statSync(dest).mode & 0o777, stream.mode)
+      stream.end()
     })
+
+    await Promise.all([endPromise1, endPromise2])
   })
 
-  test('mode on append', { skip: isWindows }, (t) => {
-    t.plan(5)
+  test('mode on append', { skip: isWindows }, async (t) => {
+    t.plan(4)
 
     const dest = file()
     fs.writeFileSync(dest, 'hello world\n', 'utf8', 0o422)
     const mode = isWindows ? 0o444 : 0o666
     const stream = new SonicBoom({ dest, append: false, mode, sync })
 
-    stream.on('ready', () => {
-      t.pass('ready emitted')
+    const endPromise1 = stream.on('ready', () => {
+      t.assert.ok('ready emitted')
     })
 
-    t.ok(stream.write('something else\n'))
+    t.assert.ok(stream.write('something else\n'))
 
     stream.flush()
 
-    stream.on('drain', () => {
-      fs.readFile(dest, 'utf8', (err, data) => {
-        t.error(err)
-        t.equal(data, 'something else\n')
-        t.equal(fs.statSync(dest).mode & 0o777, stream.mode)
-        stream.end()
-      })
+    const endPromise2 = once(stream, 'drain', () => {
+      const data = fs.readFileSync(dest, 'utf8')
+      t.assert.strictEqual(data, 'something else\n')
+      t.assert.strictEqual(fs.statSync(dest).mode & 0o777, stream.mode)
+      stream.end()
     })
+
+    await Promise.all([endPromise1, endPromise2])
   })
 }
